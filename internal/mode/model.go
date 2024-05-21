0366749d5f2b72ab11dc0944986eb22d494e36cd
ZCMATHMA <mathew.ma@zeiss.com>
2024-05-21 10:38:49 +0800

    add model and router

---
+++ internal/mode/model.go
@@ -0,0 +1,11 @@
+package mode
+
+type Model struct {
+	ID         uint32 `gorm:"primary_key" json:"id"`
+	CreatedBy  string `json:"created_by"`
+	ModifiedBy string `json:"modified_by"`
+	CreateOn   uint32 `json:"create_on"`
+	ModifiedOn uint32 `json:"modified_on"`
+	DeletedOn  uint32 `json:"deleted_on"`
+	IsDel      uint8  `json:"is_del"`
+}
